bfd46a296218ad19f70a640888bc8cf4bc22422e
Charles Zinn <charles.zinn@gmail.com>
2015-09-19 18:24:21 -0400

    Add game queues

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ r = FlaskRedis.from_custom_provider(StrictRedis, app)
 
 # -------- HACKY CONFIG -------- #
 
-EXPIRE_SECONDS = 1000
+EXPIRE_SECONDS = 10
+MIN_PLAYERS = 2
+MAX_PLAYERS = 5
 
 
 # -------- REDIS METHODS -------- #
 
-def token_key(token):
+def token_key(token): # actually username
     return "token:{0}".format(token)
 
 def updates_key(token):
@@ -25,6 +27,9 @@ def updates_key(token):
 
 def update_id_key(token):
     return "update_key:{0}".format(token)
+
+def status_key(token):
+    return "status:{0}".format(token)
 
 def create_new_token(username):
     token = str(uuid.uuid4())
@@ -44,7 +49,17 @@ def ping_token(token):
     return username
 
 def user_active(token):
-    return r.get(token_key(token))
+    username = r.get(token_key(token))
+    return username.decode("utf-8") if username else None
+
+def delete_user(token):
+    r.srem("active_users", token)
+    status = r.get(status_key(token))
+    r.delete(status_key(token))
+
+    # do stuff based on the status
+    if status and status.decode("utf-8")[:5] == "queue":
+        r.srem(status, token)
 
 # Update is a dictionary
 def push_update(token, update):
@@ -57,7 +72,7 @@ def push_update(token, update):
     r.rpush(rkey, json.dumps(update))
     return True
 
-# Gets updates after the given update
+# Gets updates since the given update id
 def get_updates(token, update_id=0):
     rkey = updates_key(token)
     idkey = update_id_key(token)
@@ -83,21 +98,109 @@ def get_updates(token, update_id=0):
 def get_active_users():
     actives = r.smembers("active_users")
     rval = []
-    pipe = r.pipeline()
     for token in actives:
         token = token.decode("utf-8")
         if user_active(token):
             rval.append(token)
         else:
-            pipe.srem("active_users", token)
-    pipe.execute()
+            delete_user(token)
     return rval
+
+def get_status(token):
+    if user_active(token):
+        status = r.get(status_key(token))
+        return status.decode("utf-8") if status else "lobby"
+    return None
+
+def get_queue(queuetype):
+    qkey = "queue:{0}".format(queuetype)
+    queue = {"users": [], "size": queuetype}
+    member_tokens = [v.decode("utf-8") for v in r.smembers(qkey)]
+    for member in member_tokens:
+        username = user_active(member)
+        if username:
+            queue["users"].append({
+                "id": short_id(member),
+                "name": username
+            })
+        else:
+            delete_user(member)
+    return queue
+
+def join_queue(token, queuetype):
+    if queuetype < MIN_PLAYERS or queuetype > MAX_PLAYERS:
+        return False
+
+    # TODO: this method has some theoretical breaking points
+    # also redundant calls to redis because hack
+
+    status = get_status(token)
+    # Ensure that they are just in the lobby
+    if status != "lobby":
+        return False
+
+    # go through the queue first and remove any inactive users
+    qkey = "queue:{0}".format(queuetype)
+    members = r.smembers(qkey)
+    for member in members:
+        member = member.decode("utf-8")
+        if not user_active(member):
+            r.srem(qkey, member)
+
+    # add them to the queue and change their status and get the new size of the queue
+    pipe = r.pipeline()
+    pipe.set(status_key(token), qkey)
+    pipe.sadd(qkey, token)
+    pipe.scard(qkey)
+    results = pipe.execute()
+
+    # if the queue isn't full, send all queue users updates about the queue
+    qsize = int(results[2]) if results[2] else 0
+    if qsize < queuetype:
+        update = get_queue(queuetype)
+        update["type"] = "queue"
+        update["status"] = qkey
+
+        member_tokens = [v.decode("utf-8") for v in r.smembers(qkey)]
+        for member in member_tokens:
+            push_update(member, update)
+    else:
+        # start the game!
+        pass
+
+    return True
+
+def leave_queue(token):
+    # ensure they're in a queue
+    status = get_status(token)
+    if status[:5] != "queue":
+        return False
+
+    queuetype = int(status.split(":")[1])
+    qkey = status
+    pipe = r.pipeline()
+    pipe.srem(qkey, token)
+    pipe.set(status_key(token), "lobby")
+    pipe.scard(qkey)
+    results = pipe.execute()
+
+    qsize = int(results[2]) if results[2] else 0
+    if qsize > 0:
+        update = get_queue(queuetype)
+        update["type"] = "queue"
+        update["status"] = qkey
+
+        member_tokens = [v.decode("utf-8") for v in r.smembers(qkey)]
+        for member in member_tokens:
+            push_update(member, update)
+
+    return True
 
 
 # -------- HELPER METHODS -------- #
 
 def short_id(token):
-    return token.split("-")[:1]
+    return token.split("-")[0]
 
 
 # -------- DECORATORS -------- #
@@ -118,11 +221,11 @@ def check_auth(f):
 # -------- ROUTES -------- #
 
 @app.route("/")
-def hello():
-    return render_template("index.html", token=session["token"] if "token" in session else None)
+def route_main():
+    return render_template("index.html", token=session["token"] if "token" in session and ping_token(session["token"]) else "")
 
 @app.route("/token")
-def get_token():
+def route_token():
     username = request.args.get("username", None)
     if username:
         # Create a token for the user
@@ -134,34 +237,71 @@ def get_token():
 
 @app.route("/updates")
 @check_auth
-def updates():
+def route_updates():
     update_id = int(request.args.get("id", 0))
     return json.dumps({"updates": get_updates(g.token, update_id)})
 
-@app.route("/username")
-@check_auth
-def username():
-    return json.dumps({"username": g.username})
-
 @app.route("/chat", methods=["POST"])
 @check_auth
-def chat():
+def route_chat():
     if not request.json["message"]:
         return json.dumps({"error": "no message"})
 
     update = {
         "type": "chat",
-        "user_id": short_id(g.token),
-        "username": g.username,
+        "user": {
+            "id": short_id(g.token),
+            "name": g.username
+        },
         "message": request.json["message"]
     }
 
     users = get_active_users()
-    print(users)
     for user in users:
         push_update(user, update)
 
     return json.dumps({"success": 1})
+
+@app.route("/getqueues")
+@check_auth
+def route_getqueues():
+    queues = []
+    for i in range(MIN_PLAYERS, MAX_PLAYERS + 1):
+        queues.append(get_queue(i))
+    return json.dumps(queues)
+
+@app.route("/joinqueue", methods=["POST"])
+@check_auth
+def route_joinqueue():
+    queuetype = request.json["queuetype"]
+    if not queuetype:
+        return json.dumps({"error": "specify queuetype"})
+    success = join_queue(g.token, queuetype)
+    if success:
+        return json.dumps({"success": 1})
+    else:
+        return json.dumps({"error": "there was a problem joining the queue"})
+
+@app.route("/leavequeue", methods=["POST"])
+@check_auth
+def route_leavequeue():
+    success = leave_queue(g.token)
+    if success:
+        push_update(g.token, {"type": "status", "status": "lobby"})
+        return json.dumps({"success": 1})
+    else:
+        return json.dumps({"error": "there was a problem leaving the queue"})
+
+@app.route("/status")
+@check_auth
+def route_status():
+    status = get_status(g.token)
+    obj = {"status": status, "username": g.username}
+    if status[:5] == "queue":
+        queue = get_queue(int(status.split(":")[1]))
+        obj["users"] = queue["users"]
+        obj["size"] = queue["size"]
+    return json.dumps(obj)
 
 
 # -------- START APP -------- #
--- static/app.js
+++ static/app.js
@@ -8,12 +8,21 @@ app.config(["$interpolateProvider", function($interpolateProvider) {
 app.controller("MainController", ["$http", "$timeout", function($http, $timeout) {
   var ctrl = this;
   ctrl.token = null;
+  ctrl.status = null;
+  ctrl.queueList = null;
+  ctrl.currentQueue = null;
 
   if (old_token.length > 0) {
-    $http.get("/username")
+    $http.get("/status")
       .then(function(response) {
         ctrl.token = old_token;
         ctrl.username = response.data.username; // should be the same
+        ctrl.status = response.data.status;
+        if (ctrl.status.substring(0, 5) == "queue") {
+          ctrl.currentQueue = {users: response.data.users, size: response.data.size};
+          console.log(ctrl.currentQueue);
+        }
+        console.log(response.data);
         startUpdates();
       }, function(response) {
         // TODO: handle error
@@ -25,6 +34,7 @@ app.controller("MainController", ["$http", "$timeout", function($http, $timeout)
       .then(function(response) {
         ctrl.token = response.data.token;
         ctrl.username = response.data.username; // should be the same
+        ctrl.status = "lobby";
         startUpdates();
       }, function(response) {
         // TODO: handle error
@@ -42,20 +52,67 @@ app.controller("MainController", ["$http", "$timeout", function($http, $timeout)
       });
   };
 
+  ctrl.joinQueue = function(queueIndex) {
+    $http.post("joinqueue", {"queuetype": ctrl.queueList[queueIndex].size})
+      .then(function(response) {
+        getUpdates(null);
+      }, function(response) {
+
+      });
+  };
+
+  ctrl.leaveQueue = function() {
+    $http.post("leavequeue", {})
+      .then(function(response) {
+        ctrl.currentQueue = null;
+        getUpdates(null);
+        ctrl.getQueues();
+      }, function(response) {
+
+      });
+  };
+
+  ctrl.getQueues = function() {
+    $http.get("getqueues")
+      .then(function(response) {
+        ctrl.queueList = response.data;
+      }, function(response) {
+
+      });
+  };
+
+  function handleUpdate(update) {
+    console.log(update);
+    if (update.status) {
+      ctrl.status = update.status;
+    }
+    if (update.type == "queue") {
+      ctrl.currentQueue = {users: update.users, size: update.size};
+    }
+  }
+
+  var last_id = 0;
+  function getUpdates(callback) {
+    $http.get("/updates?id=" + last_id)
+      .then(function(response) {
+        for (var i = 0; i < response.data.updates.length; i++) {
+          handleUpdate(response.data.updates[i]);
+          last_id = response.data.updates[i].id;
+        }
+        if (callback) {
+          callback();
+        }
+      }, function(response) {
+        // TODO: handle error
+      });
+  }
+
   function startUpdates() {
-    var last_id = 0;
+    ctrl.getQueues();
     (function tick() {
-      $http.get("/updates?id=" + last_id)
-        .then(function(response) {
-          for (var i = 0; i < response.data.updates.length; i++) {
-            console.log(response.data.updates[i]);
-            last_id = response.data.updates[i].id;
-          }
-          $timeout(tick, 1000);
-        }, function(response) {
-          // TODO: handle error
-          //$timeout(tick, 1000);
-        });
+      getUpdates(function() {
+        $timeout(tick, 1000);
+      });
     })();
   }
 }]);
